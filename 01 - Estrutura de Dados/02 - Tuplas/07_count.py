08009a839964da05c33050b198ca1df35471fb95
Lubota Ngola Joao <lubota4@gmail.com>
2024-06-16 14:40:08 -0300

    02 - Tuplas: Contando elementos de uma tupla

--- 01 - Estrutura de Dados/02 - Tuplas/07_count.py
+++ 01 - Estrutura de Dados/02 - Tuplas/07_count.py
@@ -3,8 +3,12 @@ cores = (
     "azul",
     "verde",
     "azul",
+    "uva",
+    "manga",
+    "manga",
 )
 
 print(cores.count("vermelho"))  # 1
 print(cores.count("azul"))  # 2
 print(cores.count("verde"))  # 1
+print(cores.count("manga"))  # 2
