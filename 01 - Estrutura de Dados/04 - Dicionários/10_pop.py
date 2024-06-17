dba27968ce43393f6f86e6436a442bc5d4939a21
Lubota Ngola Joao <lubota4@gmail.com>
2024-06-16 23:50:22 -0300

    04 - Dicionários: Métodos do Dicionário

---
+++ 01 - Estrutura de Dados/04 - Dicionários/10_pop.py
@@ -0,0 +1,9 @@
+contatos = {"guilherme@gmail.com": {"nome": "Guilherme", "telefone": "3333-2221"}}
+
+resultado = contatos.pop("guilherme@gmail.com")  # {'nome': 'Guilherme', 'telefone': '3333-2221'}
+print(resultado)
+
+resultado = contatos.pop("guilherme@gmail.com", {})  # {}
+resultado2 = contatos.pop("guilherme@gmail.com", "nao encontrado")  # nao encontrado
+print(resultado)
+print(resultado2)
