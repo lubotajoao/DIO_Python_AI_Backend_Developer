b7d36dabfcb09bb583f37dbabeb781041026d6a2
Lubota Ngola Joao <lubota4@gmail.com>
2024-07-11 15:47:11 -0300

    POO - Conceito de Herança 2

---
+++ 02 - Programação Orientada a Objetos/04 - Herança/01_heranca_simples.py
@@ -0,0 +1,38 @@
+class Veiculo:
+    def __init__(self, cor, placa, numero_rodas):
+        self.cor = cor
+        self.placa = placa
+        self.numero_rodas = numero_rodas
+
+    def ligar_motor(self):
+        print("Ligando o motor")
+
+    def __str__(self):
+        return f"{self.__class__.__name__}: {', '.join([f'{chave}={valor}' for chave, valor in self.__dict__.items()])}"
+
+
+class Motocicleta(Veiculo):
+    pass
+
+
+class Carro(Veiculo):
+    pass
+
+
+class Caminhao(Veiculo):
+    def __init__(self, cor, placa, numero_rodas, carregado):
+        super().__init__(cor, placa, numero_rodas)
+        self.carregado = carregado
+
+    def esta_carregado(self):
+        print(f"{'Sim' if self.carregado else 'Não'} estou carregado")
+
+
+moto = Motocicleta("Preta", "90293", 2)
+carro = Carro("Branca", "9045293", 4)
+caminhao = Caminhao("Branca", "x3d-934", 8, True)
+
+
+print(moto)
+print(carro)
+print(caminhao)
